47324b645f5eaa9ab0e68096102125b56555483c
endy <admin@site15.ru>
2017-08-12 00:51:07 +0500

    fix: Try fix error run scraper test on travis #2

--- src/plugins/scraper-ping.plugin.spec.ts
+++ src/plugins/scraper-ping.plugin.spec.ts
@@ -7,30 +7,30 @@ import { ScraperPlugin } from './scraper.plugin';
 const assert = chai.assert;
 let plugin: ScraperPlugin;
 
-describe('ScraperPlugin: ping', () => {
+describe('ScraperPlugin', () => {
     describe('without telegram', () => {
         before(function () {
             config();
             plugin = new ScraperPlugin(
                 null,
                 process.env.TELEGRAM_BOT_NAME_ALIASES.split(','),
-                'http://2whois.ru/?t=ping&data={text}',
+                process.env.SCRAPER_PING_URI,
                 +process.env.SCRAPER_PING_TIMEOUT,
                 process.env.SCRAPER_PING_CONTENT_SELECTOR,
                 +process.env.SCRAPER_PING_CONTENT_LENGTH,
                 process.env.SCRAPER_PING_SPY_WORDS.split(',')
             );
         });
-        it('should response include a message "--- google.com ping statistics ---"', (done) => {
+        it('should response include a message "--- ya.ru ping statistics ---"', (done) => {
             const msg: ITelegramBotMessage = {
-                text: 'ping google.com',
+                text: 'ping ya.ru',
                 chat: {
                     id: 'random',
                     type: 'private'
                 }
             };
             plugin.process(msg).on('message', (answer: string) => {
-                assert(answer.indexOf('--- google.com ping statistics ---') !== -1);
+                assert(answer.indexOf('--- ya.ru ping statistics ---') !== -1);
                 done();
             })
         });
