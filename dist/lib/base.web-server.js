d326a5436dedad74a10882cf0b8408ce1e720b93
endy <admin@site15.ru>
2017-08-16 18:35:40 +0500

    chore: Add debug with https://rollbar.com

--- dist/lib/base.web-server.js
+++ dist/lib/base.web-server.js
@@ -2,12 +2,16 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const express = require("express");
 const bodyParser = require("body-parser");
+const rollbar = require('rollbar');
 class BaseWebServer {
     constructor(name) {
         this.name = name;
         this.port = this.env('PORT');
         this.app = express();
         this.app.use(bodyParser.json());
+        if (this.env('DEBUG') !== 'true') {
+            this.app.use(rollbar.errorHandler(this.env('ROLLBAR_SERVER_ACCESS_TOKEN')));
+        }
         this.app.listen(this.port, () => {
             console.log(`Express server is listening on ${this.port}`);
         });
--- src/lib/base.web-server.ts
+++ src/lib/base.web-server.ts
@@ -2,6 +2,8 @@ import * as express from 'express';
 import * as bodyParser from 'body-parser';
 import { IWebServer } from './interfaces';
 
+const rollbar = require('rollbar');
+
 export class BaseWebServer implements IWebServer {
     public app: any;
     protected port: string;
@@ -9,6 +11,9 @@ export class BaseWebServer implements IWebServer {
         this.port = this.env('PORT');
         this.app = express();
         this.app.use(bodyParser.json());
+        if (this.env('DEBUG') !== 'true') {
+            this.app.use(rollbar.errorHandler(this.env('ROLLBAR_SERVER_ACCESS_TOKEN')));
+        }
         this.app.listen(this.port, () => {
             console.log(`Express server is listening on ${this.port}`);
         });
