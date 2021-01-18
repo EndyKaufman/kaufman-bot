438b30ccb6e1c319a270850c09e60fe13fd82cf7
EndyKaufman <admin@site15.ru>
2021-01-18 17:40:58 +0400

    feat(ci): update k8s build

--- k8s/build-images.sh
+++ k8s/build-images.sh
@@ -2,4 +2,5 @@ export UID=$(id -u)
 export GID=$(id -g)
 node ./k8s/generators/generator.js
 ./docker/$HOST_TYPE/docker-compose-build.sh
+mkdir -p ./tmp
 docker save $PROJECT_NAME/bot:local | gzip > ./tmp/$PROJECT_NAME-bot-$HOST_TYPE.tar.gz
