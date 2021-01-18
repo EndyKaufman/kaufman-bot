1321f08c0d879105b0cc5c21cbdccf39d86aa3e2
EndyKaufman <admin@site15.ru>
2021-01-18 19:25:44 +0400

    fix(ci): update import-images

--- k8s/import-images.sh
+++ k8s/import-images.sh
@@ -5,7 +5,7 @@ VERSION="$(cat VERSION.txt)"
 docker rmi $(docker images -f "dangling=true" -q)
 
 docker load < ./tmp/$PROJECT_NAME-bot-$HOST_TYPE.tar.gz
-ID="$(docker images | grep '$PROJECT_NAME/bot' | head -n 1 | awk '{print $3}')"
+ID="$(docker images | grep $PROJECT_NAME'/bot' | head -n 1 | awk '{print $3}')"
 docker tag "$ID" localhost:32000/$PROJECT_NAME/bot:$VERSION
 docker rmi $PROJECT_NAME/bot:local
 docker push localhost:32000/$PROJECT_NAME/bot:$VERSION
