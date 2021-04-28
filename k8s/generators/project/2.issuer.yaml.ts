10de5432d697c6cc6372da03881b692d13447202
EndyKaufman <admin@site15.ru>
2021-04-28 21:35:46 +0400

    feat: update ingress config for networking.k8s.io/v1

--- k8s/generators/project/2.issuer.yaml.ts
+++ k8s/generators/project/2.issuer.yaml.ts
@@ -4,12 +4,12 @@ export const projectIssuerYaml = {
     apiVersion: `cert-manager.io/v1alpha2`,
     kind: `ClusterIssuer`,
     metadata: {
-        namespace: `${PROJECT_NAME}-${HOST_TYPE}`,
         name: `letsencrypt-${HOST_TYPE}`,
+        namespace: `cert-manager`,
     },
     spec: {
         acme: {
-            server: `https://acme-v02.api.letsencrypt.org/directory`,
+            server: HOST_TYPE === HostType.Local ? `https://acme-staging-v02.api.letsencrypt.org/directory` : `https://acme-v02.api.letsencrypt.org/directory`,
             email: LETSENCRYPT_EMAIL,
             privateKeySecretRef: {
                 name: `letsencrypt-${HOST_TYPE}`,
