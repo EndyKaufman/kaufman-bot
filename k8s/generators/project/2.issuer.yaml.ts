304312040fd8882723336ad8d2f1cd3adea5a379
EndyKaufman <admin@site15.ru>
2021-04-28 21:41:36 +0400

    feat: update issuer script

--- k8s/generators/project/2.issuer.yaml.ts
+++ k8s/generators/project/2.issuer.yaml.ts
@@ -1,28 +1,28 @@
-import { HOST_TYPE, LETSENCRYPT_EMAIL, PROJECT_NAME } from '../constants';
+import { HostType, HOST_TYPE, LETSENCRYPT_EMAIL } from '../constants';
 
 export const projectIssuerYaml = {
-    apiVersion: `cert-manager.io/v1alpha2`,
-    kind: `ClusterIssuer`,
-    metadata: {
+  apiVersion: `cert-manager.io/v1alpha2`,
+  kind: `ClusterIssuer`,
+  metadata: {
+    name: `letsencrypt-${HOST_TYPE}`,
+    namespace: `cert-manager`,
+  },
+  spec: {
+    acme: {
+      server: HOST_TYPE === HostType.Local ? `https://acme-staging-v02.api.letsencrypt.org/directory` : `https://acme-v02.api.letsencrypt.org/directory`,
+      email: LETSENCRYPT_EMAIL,
+      privateKeySecretRef: {
         name: `letsencrypt-${HOST_TYPE}`,
-        namespace: `cert-manager`,
-    },
-    spec: {
-        acme: {
-            server: HOST_TYPE === HostType.Local ? `https://acme-staging-v02.api.letsencrypt.org/directory` : `https://acme-v02.api.letsencrypt.org/directory`,
-            email: LETSENCRYPT_EMAIL,
-            privateKeySecretRef: {
-                name: `letsencrypt-${HOST_TYPE}`,
+      },
+      solvers: [
+        {
+          http01: {
+            ingress: {
+              class: `nginx`,
             },
-            solvers: [
-                {
-                    http01: {
-                        ingress: {
-                            class: `nginx`,
-                        },
-                    },
-                },
-            ],
+          },
         },
+      ],
     },
+  },
 };
