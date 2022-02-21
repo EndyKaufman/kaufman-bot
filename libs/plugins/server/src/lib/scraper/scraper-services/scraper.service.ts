509201b1dcc7ae9e45c2a2ffb172c06fe38ee3e6
EndyKaufman <admin@site15.ru>
2022-02-21 12:56:13 +0400

    chore: add fake headers for correct work with site

--- libs/plugins/server/src/lib/scraper/scraper-services/scraper.service.ts
+++ libs/plugins/server/src/lib/scraper/scraper-services/scraper.service.ts
@@ -86,13 +86,50 @@ export class ScraperService {
     const repalcedUri = render(this.scraperConfig.uri, replaceVariables);
     // const replacedText = render(text, replaceVariables);
 
+    // Helper functions to get a random item from an array
+    const sample = (array) => array[Math.floor(Math.random() * array.length)];
+
+    const headers = [
+      {
+        Accept:
+          'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+        'Accept-Encoding': 'gzip, deflate, br',
+        'Accept-Language': 'en-US,en;q=0.9',
+        'Sec-Ch-Ua':
+          '"Chromium";v="92", " Not A;Brand";v="99", "Google Chrome";v="92"',
+        'Sec-Ch-Ua-Mobile': '?0',
+        'Sec-Fetch-Dest': 'document',
+        'Sec-Fetch-Mode': 'navigate',
+        'Sec-Fetch-Site': 'none',
+        'Sec-Fetch-User': '?1',
+        'Upgrade-Insecure-Requests': '1',
+        'User-Agent':
+          'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.107 Safari/537.36',
+      },
+      {
+        Accept:
+          'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+        'Accept-Encoding': 'gzip, deflate, br',
+        'Accept-Language': 'en-US,en;q=0.5',
+        'Sec-Fetch-Dest': 'document',
+        'Sec-Fetch-Mode': 'navigate',
+        'Sec-Fetch-Site': 'none',
+        'Sec-Fetch-User': '?1',
+        'Upgrade-Insecure-Requests': '1',
+        'User-Agent':
+          'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:90.0) Gecko/20100101 Firefox/90.0',
+      },
+    ];
+
     const axiosInstance = axios.create({
-      timeout: this.scraperConfig.timeout,
+      timeout: this.scraperConfig.timeout || 3000,
       responseEncoding: this.scraperConfig.contentCodepage || 'binary',
     });
 
     try {
-      const response = await axiosInstance.get(repalcedUri);
+      const response = await axiosInstance.get(repalcedUri, {
+        headers: sample(headers),
+      });
 
       const $ = cheerio.load(response.data);
       let content = this.scraperConfig.contentSelector
@@ -100,7 +137,6 @@ export class ScraperService {
         .map((selector: string) => htmlToText.fromString($(selector).html()))
         .join('\n\n');
 
-      console.log(content);
       const enc =
         charset(response.headers, response.data) ||
         jschardet.detect(response.data).encoding.toLowerCase();
