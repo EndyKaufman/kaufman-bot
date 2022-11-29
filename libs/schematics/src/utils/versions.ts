34843be39431b7760f8137b3266438c637217c56
EndyKaufman <admin@site15.ru>
2022-11-29 18:39:31 +0400

    fix: move migrations to libs, added last version of libs on generate code with schematics

--- libs/schematics/src/utils/versions.ts
+++ libs/schematics/src/utils/versions.ts
@@ -1,7 +1,16 @@
+import { readFileSync } from 'fs';
+import { join } from 'path';
+
 export const nxVersion = '15.0.13';
 
 export const reflectMetadataVersion = '^0.1.13';
-export const kaufmanBotVersion = '*';
 export const nestJsVersion = '^9.0.0';
 export const rxjsVersion = '^7.0.0';
 export const tsLibVersion = '^2.3.0';
+
+export const kaufmanBotVersion =
+  process.env.KAUFMAN_BOT_VERSION ||
+  JSON.parse(
+    readFileSync(join(__dirname, '..', '..', 'package.json')).toString()
+  ).version ||
+  '*';
